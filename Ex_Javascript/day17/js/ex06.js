752da09d73565cec75b9b2ccd53efe2d4ba9f71a
ha752002 <honggha75@gmail.com>
2023-08-10 21:20:14 +0700

    bt

--- Ex_Javascript/day17/js/ex06.js
+++ Ex_Javascript/day17/js/ex06.js
@@ -2,3 +2,16 @@
 // Học viên sử dụng kiến thức
 //  đã học về vòng lặp,
 //  câu lệnh rẽ nhánh để vẽ bàn cờ vua
+
+var boardSize = 8;
+var chessboard = document.getElementById("chessboard");
+var chessboardHTML = "";
+
+for (var row = 1; row <= boardSize; row++) {
+    for (var col = 1; col <= boardSize; col++) {
+        var squareColor = (row + col) % 2 === 0 ? "white-square" : "black-square";
+        chessboardHTML += `<div class="${squareColor}"></div>`;
+    }
+}
+
+chessboard.innerHTML = chessboardHTML;
