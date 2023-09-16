60922ea3fb7f7f45a26af5fef3eac86e0da7b7c6
ha752002 <honggha75@gmail.com>
2023-09-16 13:13:56 +0700

    day 29

---
+++ Lesson-javascript/dom/js/ex04.js
@@ -0,0 +1,11 @@
+
+var preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', function(){
+    // console.log('tải xong ');
+    // var img = document.querySelector('img');
+    // console.log(img.width);
+
+    preloader.remove();
+});
+
