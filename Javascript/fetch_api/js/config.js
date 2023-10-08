2965910bdc3d2996dea6d5f7ebddf375ba0d52e3
ha752002 <honggha75@gmail.com>
2023-10-08 20:45:01 +0700

    buoi 36 -01

--- Javascript/fetch_api/js/config.js
+++ Javascript/fetch_api/js/config.js
@@ -1,5 +1,7 @@
 export const config = {
     SERVER_API: "http://localhost:3000",
+    PAGE_LIMIT: 7,
 };
 
-console.log(SERVER_API);
+
+// console.log(SERVER_API);
--- Javascript/fetch_api/js/ex03.js
+++ Javascript/fetch_api/js/ex03.js
@@ -1,117 +1,120 @@
 import { config } from "./config.js";
 import { client } from "./client.js";
+// import { PAGE_LIMIT } from "./config.js";
 
-// const { SERVER_API } = config;
+const { PAGE_LIMIT } = config;
+const renderPaginate = (totalPage) => {
+  const paginateEl = document.querySelector(".paginate");
 
-const render = (posts) => {
-    const postEl = document.querySelector(".posts");
+  paginateEl.innerText = "";
+  if (totalPage > 1) {
+    const spanPrev = document.createElement("span");
 
-    postEl.innerText = '';
-    if (posts.length) {
-        posts.forEach(({ id, title, excerpt }) => {
-            const postItem = document.createElement("div");
-            postItem.classList.add('post-item');
-
-            const h2 = document.createElement("h2");
-            const a = document.createElement("a");
-
-            a.innerText = title;
-            a.href = "#";
-            h2.append(a);
-            postItem.append(h2);
-
-
-            const p = document.createElement("p");
-            p.innerText = excerpt;
-            postItem.append(p);
-
-
-            postEl.append(postItem);
-        })
+    if (currentPage > 1) {
+      const aPrev = document.createElement("a");
+      aPrev.href = "#";
+      aPrev.addEventListener("click", (e) => {
+        e.preventDefault();
+        goPage(--currentPage);
+      });
+      aPrev.innerText = "Prev";
+      spanPrev.append(aPrev);
+      paginateEl.append(spanPrev);
     }
 
+    for (let page = 1; page <= totalPage; page++) {
+      // console.log(currentPage);
+      const span = document.createElement("span");
+      if (+page === +currentPage) {
+        span.classList.add("active");
+      }
+      const a = document.createElement("a");
+      a.href = "#";
+      a.innerText = page;
+      a.addEventListener("click", (e) => {
+        e.preventDefault();
+        goPage(page);
+      });
+      span.append(a);
+      paginateEl.append(span);
+    }
 
+    if (currentPage < totalPage) {
+      const spanNext = document.createElement("span");
+      const aNext = document.createElement("a");
+      aNext.href = "#";
+      aNext.addEventListener("click", (e) => {
+        e.preventDefault();
+        goPage(++currentPage);
+      });
+      aNext.innerText = "Next";
+      spanNext.append(aNext);
+      paginateEl.append(spanNext);
+    }
+  }
+};
+const goPage = (page) => {
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
 
-}
+  currentPage = page;
 
-const getPost = async (query = {}) => {
-
-    const queryString = new URLSearchParams(query).toString();
-
-    const { response, data } = await client.get(`/posts?${queryString}`);
-    // console.log(data);
-    render(data);
-}
-let sort = "id";
-let order = "desc";
-let keyword = "";
-
-
-getPost({
+  getPosts({
     _sort: sort,
     _order: order,
+    _limit: limit,
+    _page: currentPage,
+    q: keyWord,
+  });
+};
+
+const render = (posts) => {
+  const postsEl = document.querySelector(".posts");
+  postsEl.innerText = ``;
+  if (posts.length) {
+    posts.forEach(({ id, title, excerpt }) => {
+      const postsItem = document.createElement("div");
+      postsItem.classList.add("post-item");
+      const h2 = document.createElement("h2");
+      const a = document.createElement("a");
+      a.innerText = title;
+      a.href = "#";
+      h2.append(a);
+      postsItem.append(h2);
+
+      const p = document.createElement("p");
+      p.innerText = excerpt;
+      postsItem.append(p);
+      postsEl.append(postsItem);
+    });
+  }
+};
+
+const getPosts = async (query = {}) => {
+  const queryString = new URLSearchParams(query).toString();
+  const { response, data } = await client.get(`/posts?${queryString}`);
+
+  const total = response.headers.get("x-total-count");
+
+  const totalPage = Math.ceil(total / PAGE_LIMIT);
+  renderPaginate(totalPage);
+
+  render(data);
+};
+
+let sort = "id";
+let order = "desc";
+let keyWord = "";
+let limit = PAGE_LIMIT;
+let currentPage = 1;
+
+getPosts({
+  _sort: sort,
+  _order: order,
+  _limit: limit,
+  _page: currentPage,
 });
 
-const searchForm = document.querySelector('.search');
-
-searchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    keyword = e.target.children[0].value;
-    getPost({
-        q: keyword,
-        _sort: sort,
-        _order: order
-    })
-})
-
-
-const sortByEl = document.querySelector('.sort-by');
-sortByEl.addEventListener('change', (e) => {
-    order = e.target.value === "latest" ? "desc" : "asc";
-
-    getPost({
-        q: keyword,
-        __sort: 'sort',
-        _order: 'order',
-    })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* <div class="post-item">
-          <h2><a href="#">Tiêu đề bài viết</a></h2>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores
-            nihil error voluptatibus natus obcaecati at, minima quisquam dolores
-            quidem rerum.
-          </p>
-        </div>
-        <div class="post-item">
-          <h2><a href="#">Tiêu đề bài viết</a></h2>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores
-            nihil error voluptatibus natus obcaecati at, minima quisquam dolores
-            quidem rerum.
-          </p>
-        </div>
-        <div class="post-item">
-          <h2><a href="#">Tiêu đề bài viết</a></h2>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores
-            nihil error voluptatibus natus obcaecati at, minima quisquam dolores
-            quidem rerum.
-          </p>
-        </div> */
+const searchForm = document.querySelector(".search");
