55d122879c18a721a1ddc318421501f7c66509f1
ha752002 <honggha75@gmail.com>
2023-11-04 07:55:30 +0700

    fix loading

--- Ex_ReactJs/day42/src/components/Loading/Loading.jsx
+++ Ex_ReactJs/day42/src/components/Loading/Loading.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { ThreeDots } from 'react-loader-spinner';
-import Styles from './loading.module.scss'; // Đảm bảo rằng bạn đã import đúng tệp CSS
+import Styles from './loading.module.scss';
+import clsx from 'clsx';
 
-export default function Loading({ visible }) {
+export default function loading({ visible }) {
     return (
-        // Sử dụng một div với lớp CSS overlay để tạo overlay
-        <div className={Styles.overlay}>
-            <ThreeDots
-                height={80}
-                width={80}
-                radius={9}
-                color="#4fa94d"
-                ariaLabel="three-dots-loading"
-                visible={visible}
-            />
-        </div>
+        <ThreeDots
+            height="80"
+            width="80"
+            radius="9"
+            color="#4fa94d"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{
+                position: 'fixed',
+                inset: 0,
+                width: '100vw',
+                height: '100vh',
+                zIndex: 9999,
+                alignItems: 'center',
+                justifyContent: 'center',
+                display: 'flex',
+            }}
+            // wrapperClassName={clsx(Styles.loading)}
+            visible={visible}
+            wrapperClassName={clsx(Styles.overlay)}
+        />
     );
 }
