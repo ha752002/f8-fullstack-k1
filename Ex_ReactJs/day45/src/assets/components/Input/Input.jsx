828729679590a1c108737789beb6770d12cf44e6
ha752002 <honggha75@gmail.com>
2023-11-10 15:44:24 +0700

    fix sass

--- Ex_ReactJs/day45/src/assets/components/Input/Input.jsx
+++ Ex_ReactJs/day45/src/assets/components/Input/Input.jsx
@@ -14,21 +14,25 @@ export default function Form(props) {
                 <div className={clsx(Styles.form_group)} style={{ margin: '10px 0px' }}>
                     <div role="group" style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
                         <label htmlFor="number">Hãy thử nhập một số</label>
-                        <input
-                            onChange={(e) => {
-                                props.onChange(e.target.value);
-                            }}
-                            onKeyDown={(e) => {
-                                props.onkeyDownInput(e.key);
-                            }}
-                            className={clsx(Styles.custom_input)}
-                            value={props.value}
-                            type="text"
-                            autoComplete="off"
-                            name="number"
-                            placeholder="Thử một số"
-                            id="number"
-                        />
+                        {props.isOutOfTurn ? (
+                            <input
+                                onChange={(e) => {
+                                    props.onChange(e.target.value);
+                                }}
+                                onKeyDown={(e) => {
+                                    props.onkeyDownInput(e.key);
+                                }}
+                                className={clsx(Styles.custom_input)}
+                                value={props.value}
+                                type="text"
+                                autoComplete="off"
+                                name="number"
+                                placeholder="Thử một số"
+                                id="number"
+                            />
+                        ) : (
+                            <></>
+                        )}
                     </div>
                     <button className={clsx(Styles.btn)} type="submit">
                         {props.isOutOfTurn ? 'Chơi lại' : 'Kiểm tra'}
