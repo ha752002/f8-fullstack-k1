fa4aea14e58b6a296159974822b1cc9eeacb625e
ha752002 <honggha75@gmail.com>
2023-11-11 22:51:37 +0700

    fix regex

--- Ex_ReactJs/day45/src/components/ProgressBar/ProgressBar.jsx
+++ Ex_ReactJs/day45/src/components/ProgressBar/ProgressBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function () {
+    const rate = 100;
     return (
         <>
             <div class="h-3"></div>
--- Ex_ReactJs/day45/src/validation/formValidation.js
+++ Ex_ReactJs/day45/src/validation/formValidation.js
@@ -2,7 +2,7 @@
 import { ruleConfig } from "../config/ruleConfig";
 export const formValidate = (value) => {
     const numberDigits = ruleConfig.MAX_NUMBER.toString().length;
-    const pattern = `^\\d{0,${numberDigits}}?$`;
+    const pattern = `^(?:[1-9]\\d{0,${numberDigits - 1}})?$`;
     const regex = new RegExp(pattern)
     // console.log(regex);
     var check = regex.test(value);
