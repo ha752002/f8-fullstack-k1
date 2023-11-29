73bf09e1804eed2a86304275bfde2f7e9e671c91
ha752002 <honggha75@gmail.com>
2023-11-29 14:24:57 +0700

    add api routers

--- Ex_NextJs/day49/app/providers.tsx
+++ Ex_NextJs/day49/app/providers.tsx
@@ -5,6 +5,7 @@ import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from 'next/navigation'
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import Toast from "@/components/toast";
 
 export interface ProvidersProps {
 	children: React.ReactNode;
@@ -16,6 +17,7 @@ export function Providers({ children, themeProps }: ProvidersProps) {
 
 	return (
 		<NextUIProvider navigate={router.push}>
+			<Toast/>
 			<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
 		</NextUIProvider>
 	);
--- Ex_NextJs/day49/utils/email.ts
+++ Ex_NextJs/day49/utils/email.ts
@@ -1,10 +1,12 @@
 import emailjs from '@emailjs/browser';
+import {toast} from "react-toastify";
 
 export const sendEmail = (form: HTMLFormElement) => {
     console.log(form)
     emailjs.sendForm(process.env.NEXT_PUBLIC_SERVICE_ID ?? "", process.env.NEXT_PUBLIC_TEMPLATE_ID ?? "", form, process.env.NEXT_PUBLIC_PUBLIC_KEY ?? "")
         .then((result) => {
             console.log(result.text);
+            toast("Thanh toán thành công")
         }, (error) => {
             console.log(error.text);
         });
