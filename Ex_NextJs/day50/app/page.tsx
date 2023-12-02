c382f4e9ff3d1f64aafd4168d037bb03eb24c4ee
ha752002 <honggha75@gmail.com>
2023-12-02 12:23:23 +0700

    fix authOption

--- Ex_NextJs/day50/app/page.tsx
+++ Ex_NextJs/day50/app/page.tsx
@@ -1,5 +1,5 @@
 import {getServerSession} from "next-auth";
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import {authOptions} from "@/app/option/authOptions";
 
 
 export default async function Home() {
